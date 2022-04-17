bb026691f44cfc9ace27337702aadcf779bd65cb
Maksym Sen <Maksymsen@hotmail.com>
2022-04-17 18:29:30 -0400

    fixing up the restaurant service

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { RestaurantService } from '../services/data.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  restaurants = this.restaurantService.getRestaurants();
+  addedRestaurants = [];
 
-  constructor(private route: Router) { }
+  constructor(private route: Router, private restaurantService: RestaurantService) {
+    for (const restaurant of this.restaurants) {
+      if (restaurant.added) {
+        this.addedRestaurants.push(restaurant);
+      }
+    }
+  }
 
   aboutPage() {
     this.route.navigate(['/home']);
